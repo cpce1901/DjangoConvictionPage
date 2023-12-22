420cce5a5d32b43acd5438853614e64a8ab17b87
cpce1901 <ircd.claudio@gmail.com>
2023-12-22 12:17:12 -0300

    updateContact

--- appConv/apps/contact/viewsets.py
+++ appConv/apps/contact/viewsets.py
@@ -1,26 +1,37 @@
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import GenericViewSet
 from .serializer import MessageSerializer, MobileTokenSerializer
 
 
-class MessageViewSet(ModelViewSet):
+class MessageViewSet(GenericViewSet):
     serializer_class = MessageSerializer
     queryset = MessageSerializer.Meta.model.objects.all()
 
-    def create(self, request):
-        serializer = self.serializer_class(data=request.data)
+    def list(self, request):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk):
+        item = self.get_object()
+        serializer = self.get_serializer(item)
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        item = self.get_object()
+        serializer = self.get_serializer(instance=item, data=request.data)
 
         if serializer.is_valid():
             serializer.save()
             return Response(
-                {"message": "Provider guardado con exito... "},
+                {"message": "message update... "},
                 status=status.HTTP_201_CREATED,
             )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class MobileTokenViewSet(ModelViewSet):
+class MobileTokenViewSet(GenericViewSet):
     serializer_class = MobileTokenSerializer
     queryset = MobileTokenSerializer.Meta.model.objects.all()
 
@@ -28,36 +39,32 @@ class MobileTokenViewSet(ModelViewSet):
         serializer = self.serializer_class(data=request.data)
 
         if serializer.is_valid():
-            serializer.save()
+            instance = serializer.save()
+            id = instance.id
             return Response(
-                {"message": "Provider guardado con exito... "},
+                {"id": id, "message": "Token guardado con exito... "},
                 status=status.HTTP_201_CREATED,
             )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def destroy(self, request, pk=None):
-        message = self.queryset.get(pk=pk)
-        if message:
-            message.delete()
-            response = {"message": "Mobile-Token eliminado correctamente."}
-            return Response(response, status=status.HTTP_200_OK)
-        return Response(
-            {"error": "No existe el Mobile-Token en los datos..."},
-            status=status.HTTP_400_BAD_REQUEST,
-        )
-
-    def list(self, request):
-        response = {"message": "Delete function is not offered in this path."}
-        return Response(response, status=status.HTTP_403_FORBIDDEN)
-
-    def retrieve(self, request, pk=None):
-        response = {"message": "Delete function is not offered in this path."}
-        return Response(response, status=status.HTTP_403_FORBIDDEN)
+    def retrieve(self, request, pk):
+        item = self.get_object()
+        serializer = self.get_serializer(item)
+        return Response(serializer.data)
 
     def update(self, request, pk=None):
-        response = {"message": "Delete function is not offered in this path."}
-        return Response(response, status=status.HTTP_403_FORBIDDEN)
+        item = self.get_object()
+        serializer = self.get_serializer(instance=item, data=request.data)
 
-    def partial_update(self, request, pk=None):
-        response = {"message": "Delete function is not offered in this path."}
-        return Response(response, status=status.HTTP_403_FORBIDDEN)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                {"message": "message update... "},
+                status=status.HTTP_201_CREATED,
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request):
+        item = self.get_object()
+        item.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
